88bf929e1dea88bbe7ab3c5e7e1326fae158ab67
Andrey Dagaev <dandr212@gmail.com>
2022-12-20 22:51:02 +0300

    add svg-circles and second-slide content

--- src/components/MainPage/Slides/FirstSlideComponent.vue
+++ src/components/MainPage/Slides/FirstSlideComponent.vue
@@ -1,7 +1,8 @@
 <template>
+
   <div class="main-wrapper">
     <!-- <img class="bg-svg" src="@/assets/mainPageImages/firstSlide/svgbg.svg" alt=""> -->
-    <h1>Функционал</h1>
+    <h1 class="header-title">Функционал</h1>
     <div class="function-container">
       <div class="top-row">
         <div class="top-card">
@@ -50,6 +51,9 @@
         </div>
       </div>
     </div>
+    <img class="bigdarkcircle" src="@/assets/mainPageImages/firstSlide/bigdarkcircle.svg" alt="">
+    <img class="middarkcircle" src="@/assets/mainPageImages/firstSlide/middarkcircle.svg" alt="">
+    <img class="circlepatern" src="@/assets/mainPageImages/firstSlide/circlepatern.svg" alt="">
     
   </div>
 </template>
@@ -62,31 +66,46 @@ export default {
 
 <style scoped>
 .main-wrapper {
+  position: relative;
   display: flex;
   justify-content: center;
   margin: auto;
   min-width: 100%;
-  /* background: #B1A9FF; */
-  background: #B1A9FF url(@/assets/mainPageImages/firstSlide/svgbg.svg); /* Фоновый цвет и фоновый рисунок*/
+  background: #B1A9FF;
   flex-direction: column;
   margin: 0;
   height: 100vh;
 }
 
+.header-title {
+  position: absolute;
+  margin-left: auto;
+  margin-right: auto;
+  left: 0;
+  right: 0;
+  top: 10px;
+  text-align: center;
+  font-size: 40px;
+  font-weight: bold;
+  color: #fff;
+  /* margin-bottom: 70px; */
+}
+
 .function-container {
   background-color: #fff;
   border-radius: 20px;
-  margin: 0 10vw;
+  margin: auto 14vw;
   display: flex;
   flex-direction: column;
+  z-index: 1;
   
 }
 .top-row {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  padding-top: 80px;
-  margin: 40px 10vw;
+  padding-top: 50px;
+  margin: 40px 5vw;
   
 }
 
@@ -94,8 +113,8 @@ export default {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  padding-bottom: 80px;
-  margin: 40px 10vw;
+  padding-bottom: 50px;
+  margin: 40px 5vw;
 }
 
 .bottom-card, .top-card {
@@ -109,5 +128,29 @@ export default {
   -webkit-box-shadow: 0px 5px 24px -4px rgba(65, 0, 90, 0.39);
   -moz-box-shadow: 0px 5px 24px -4px rgba(65, 0, 90, 0.39);
   box-shadow: 0px 5px 24px -4px rgba(65, 0, 90, 0.39);
+}
+
+.bigdarkcircle {
+  position: absolute;
+  width: 370px;
+  height: 370px;
+  top: 50px;
+  left: 115px;
+}
+
+.middarkcircle {
+  position: absolute;
+  width: 240px;
+  height: 240px;
+  bottom: 100px;
+  left: 600px;
+}
+
+.circlepatern {
+  position: absolute;
+  width: 570px;
+  height: 240px;
+  bottom: 100px;
+  right: 120px;
 }
 </style>
--- src/components/MainPage/Slides/SecondSlideComponent.vue
+++ src/components/MainPage/Slides/SecondSlideComponent.vue
@@ -12,27 +12,27 @@
     <div class="right-block">
       <div class="images-wrapper">
         <div class="img-card">
-          <img src="" alt="">
+          <img src="@/assets/mainPageImages/secondSlide/cards/first.png" alt="">
           <p>Фрукты</p>
         </div>
         <div class="img-card">
-          <img src="" alt="">
+          <img src="@/assets/mainPageImages/secondSlide/cards/second.png" alt="">
           <p>Рыба</p>
         </div>
         <div class="img-card">
-          <img src="" alt="">
+          <img src="@/assets/mainPageImages/secondSlide/cards/third.png" alt="">
           <p>Орехи</p>
         </div>
         <div class="img-card">
-          <img src="" alt="">
+          <img src="@/assets/mainPageImages/secondSlide/cards/fourth.png" alt="">
           <p>Яйца и молочные</p>
         </div>
         <div class="img-card">
-          <img src="" alt="">
+          <img src="@/assets/mainPageImages/secondSlide/cards/fifth.png" alt="">
           <p>Мясо птицы</p>
         </div>
         <div class="img-card">
-          <img src="" alt="">
+          <img src="@/assets/mainPageImages/secondSlide/cards/sixth.png" alt="">
           <p>Полезные завтраки</p>
         </div>
       </div>
@@ -40,6 +40,9 @@
         <button>Смотреть рецепты</button>
       </div>
     </div>
+    <img class="bigcircle" src="@/assets/mainPageImages/secondSlide/bigcircle.svg" alt="">
+    <img class="mediumcircle" src="@/assets/mainPageImages/secondSlide/mediumcircle.svg" alt="">
+    <img class="smallcircle" src="@/assets/mainPageImages/secondSlide/smallcircle.svg" alt="">
   </div>
 </template>
 
@@ -51,17 +54,21 @@ export default {
 
 <style scoped>
 .main-wrapper {
+  position: relative;
   background-color: #fff;
   display: flex;
   flex-direction: row;
   justify-content: space-around;
   height: 100vh;
+  margin: 70px auto 0 auto;
+  max-width: 79vw;
 }
 .left-block {
   display: flex;
   justify-content: center;
   background: #fff url(@/assets/mainPageImages/secondSlide/productsblockbg.png);
   margin: 7vh 0;
+  z-index: 2;
 }
 
 .content-block {
@@ -76,5 +83,37 @@ export default {
   color: #fff;
   text-align: center;
   margin: auto
+}
+.right-block {
+  margin: 7vh 30px;
+  z-index: 2;
+}
+.images-wrapper {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.mediumcircle {
+  position: absolute;
+  width: 150px;
+  height: 150px;
+  top: 200px;
+  right: 40px;
+}
+.bigcircle {
+  position: absolute;
+  width: 300px;
+  height: 300px;
+  bottom: 0px;
+  left: 520px;
+  
+}
+.smallcircle {
+  position: absolute;
+  width: 110px;
+  height: 110px;
+  top: 50px;
+  left: -30px;
 }
 </style>
